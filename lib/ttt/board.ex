63b0df399bc17489ccba27bf89d04ea8ec9c7569
Sam Jones <sam.jones494@googlemail.com>
2020-01-06 13:32:42 +0000

    occupy a space on the board

--- lib/ttt/board.ex
+++ lib/ttt/board.ex
@@ -2,4 +2,8 @@ defmodule Board do
   def create() do
     [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
+
+  def create(board, space) do
+    List.replace_at(board, space, "X")
+  end
 end
--- test/board_test.exs
+++ test/board_test.exs
@@ -5,6 +5,11 @@ defmodule BoardTest do
     test "create/0 creates an empty board" do
       assert Board.create() == [1, 2, 3, 4, 5, 6, 7, 8, 9]
     end
+
+    test "create/2 creates an updated board" do
+      board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      assert Board.create(board, 0) == ["X", 2, 3, 4, 5, 6, 7, 8, 9]
+    end
   end
 
 end
