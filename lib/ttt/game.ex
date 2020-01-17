d34effb4c680f28b3d0c8c9402194049ddfb7976
Sam Jones <sam.jones494@googlemail.com>
2020-01-17 10:02:20 +0000

    pass next player marker in private game state

--- lib/ttt/game.ex
+++ lib/ttt/game.ex
@@ -5,7 +5,7 @@ defmodule Ttt.Game do
 
   def play(opponent, _space=nil, _game_id=nil) do
     board = Board.create()
-    game_id = GameStore.start(%{board: board, opponent: opponent, current_player: "X"})
+    game_id = GameStore.start(%{board: board, opponent: opponent, current_player: "X", next_player: "O"})
     %{board: board, game_id: game_id_to_string(elem(game_id, 1))}
   end
 
@@ -28,7 +28,7 @@ defmodule Ttt.Game do
   defp run_turn(previous_state, space, game_id) do
     new_board = place_marker(previous_state, space)
     game_over_status = check_for_game_over(new_board)
-    update_game_store(new_board, previous_state.opponent ,set_next_player_marker(previous_state.current_player), game_id)
+    update_game_store(%{board: new_board, opponent: previous_state.opponent, current_player: previous_state.next_player, next_player: previous_state.current_player, game_id: game_id})
 
     %{board: new_board, game_id: game_id_to_string(game_id), game_over: game_over_status.game_over, message: game_over_status.message}
   end
@@ -48,17 +48,13 @@ defmodule Ttt.Game do
     if Board.is_full?(board) or Board.is_won?(board), do: true, else: false
   end
 
-  defp set_next_player_marker(current_player) do
-    if current_player == "X", do: "O", else: "X"
-  end
-
   defp set_error_message(state, game_id) do
     state
     |> Map.merge(%{message: "Please select an available space", game_id: game_id_to_string(game_id)})
   end
 
-  defp update_game_store(board, opponent, player, game_id) do
-    GameStore.update(game_id ,%{board: board, opponent: opponent, current_player: player})
+  defp update_game_store(state) do
+    GameStore.update(state.game_id ,%{board: state.board, opponent: state.opponent, current_player: state.current_player, next_player: state.next_player})
   end
 
   defp game_id_to_string(game_id) do
